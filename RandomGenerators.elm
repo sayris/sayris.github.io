910df79b4e5673a095d3a6f4673aa601a562e4f3
Sayri <sayris@uchicago.edu>
2016-07-03 21:27:54 -0600

    deleting the duplicated files

--- RandomGenerators.elm
+++
@@ -1,9 +0,0 @@
-module RandomGenerators where 
-
-import Random 
-
-
-genRIndex : Random.Seed -> Int -> (List Float, Random.Seed)
-genRIndex s n =
-    let floatList n = Random.list n (Random.float 0 1) in 
-    Random.generate floatList s 
